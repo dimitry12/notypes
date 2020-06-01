8968d7ab9fe24e033ac687365a5ef363ac9b2f9d
Dzmitry Pletnikau <dzmitry@pletnikau.com>
2020-06-01 00:19:33 -0600

    sketched out some types

--- notypes/__init__.py
+++ notypes/__init__.py
@@ -1,1 +1,41 @@
-from .core import hmm
+from typing import (
+    Protocol,
+    NewType,
+    Mapping,
+    List
+)
+from abc import (
+    abstractclassmethod,
+    abstractstaticmethod,
+    abstractmethod,
+    abstractproperty
+)
+# https://www.python.org/dev/peps/pep-0544/
+
+class StructuralEquation(Protocol):
+    @abstractproperty
+    alias: str
+
+class Variable
+
+class Value(Protocol):
+    pass
+
+Observation = NewType('Observation', Mapping[Variable, Value])
+Intervention = NewType('Intervention', Mapping[Variable, Value])
+
+class FitReport(Protocol):
+    @abstractproperty
+    likelihoodBefore: float
+
+    @abstractproperty
+    likelihoodAfter: float
+
+    @abstractproperty
+    updatedEquations: List[StructuralEquation]
+
+
+class Model(Protocol):
+    @abstractmethod
+    def fit(self, obs: Observation, act: Intervention) -> FitReport:
+        raise NotImplementedError
